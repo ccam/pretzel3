2cc18f731128e24d0e7dc645bff3e3463508b29b
ccam <c.mcdonald0625@gmail.com>
2017-01-04 20:47:15 -0500

    add schedule page

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,26 +3,68 @@ $(document).ready(function() {
   var theID; //holds the id of the menu item that was clicked 
 
   $('.close').click(function() {
-    this.hide()
+    $(this).hide()
   });
   $('.itemDetail').hide();
 
+
+  /* This gets the ID of the button that was clicked 
+  Need to add btn class back to HTML
   $('.btn').click(function() {
     theID = $(this).attr('id');
-    console.log(theID);
+    console.log( theID );
+
+  }); */
+
+//For the Pretzel Section
+  $('#a').click(function() {
+    $('#detailOne').slideToggle();
   });
 
+  $('#b').click(function() {
+    $('#detailTwo').slideToggle();
+  });
 
+  $('#c').click(function() {
+    $('#detailThree').slideToggle();
+  });
 
+  $('#d').click(function() {
+    $('#detailFour').slideToggle();
+  });
 
+  $('#e').click(function() {
+    $('#detailFive').slideToggle();
+  });
 
-/*
+  $('#f').click(function() {
+    $('#detailSix').slideToggle();
+  });
 
+  // For the Dip No functional change from above.
+  $('#g').click(function() {
+    $('#detailSeven').slideToggle();
+  });
 
- 
+  $('#h').click(function() {
+    $('#detailEight').slideToggle();
+  });
 
-  $('.item2').click(function() {
-    $('#detalTwo').slideToggle();
+  $('#i').click(function() {
+    $('#detailNine').slideToggle();
+  });
+
+  $('#j').click(function() {
+    $('#detailTen').slideToggle();
+  });
+
+  $('#k').click(function() {
+    $('#detailEleven').slideToggle();
+  });
+
+  $('#l').click(function() {
+    $('#detailTwelve').slideToggle();
   }); 
-*/
+
+
 });
